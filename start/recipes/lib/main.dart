f045678018393f43b1d2ee8755aa9434ca6c2d92
DiemHong <diemsdh@gmail.com>
2022-07-29 07:47:09 -0500

    update

--- start/recipes/lib/main.dart
+++ start/recipes/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const RecipeApp());
 }
-
 class RecipeApp extends StatelessWidget {
   const RecipeApp({Key? key}) : super(key: key);
 
