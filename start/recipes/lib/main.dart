60169ca132038852c9ee328a40d551031115a871
DiemHong <diemsdh@gmail.com>
2022-08-04 06:08:56 +0700

    Update

--- start/recipes/lib/main.dart
+++ start/recipes/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
 }
+
+
 class RecipeApp extends StatelessWidget {
   const RecipeApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
@@ -20,48 +24,79 @@ class RecipeApp extends StatelessWidget {
       home: const MyHomePage(title: 'Recipe Calculator'),);
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  @override  State<MyHomePage> createState() => _MyHomePageState();
 }
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-         _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> { 
   @override
   Widget build(BuildContext context) {    
     return Scaffold(
       appBar: AppBar(       
         title: Text(widget.title),
+                  ),
+        body: SafeArea(
+        // 4
+          child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+          // 7
+          return GestureDetector(
+              // 8
+              onTap: () {
+                // 9
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      // 10
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              // 11
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+          },
+        ),
       ),
-      body: Center(
-      
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+    );
+  }
+          Widget buildRecipeCard(Recipe recipe) {
+            return Card(
+                  // 1
+                  elevation: 2.0,
+                  // 2
+                  shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+                  // 3
+                  child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  // 4
+                  child: Column(
+                  children: <Widget>[
+                  Image(image: AssetImage(recipe.imageUrl)),
+                  // 5
+                  const SizedBox(
+                  height: 20.0,
+                  ),
+                  // 6
+                  Text(
+                    recipe.label,
+                    style: const TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: 'Palatino',  ),
+                      )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
-  }
+   }          
+          
 }
